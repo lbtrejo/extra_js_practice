ce05e68b93e1c4948ccf7f3d0a94c115a2a3e9c4
Levi Trejo <levi.b.trejo@gmail.com>
2020-08-05 21:03:09 -0500

    Sales Tax output working and solved, final cleanup refactor pending

--- js/sales-tax-challenge.js
+++ js/sales-tax-challenge.js
@@ -11,6 +11,11 @@
 // Input Item object structure:
 // Item Types = ["book", "food", "medical", "misc"]
 
+// Items to return:
+// 1. each Items subtotal
+// 2. the total sales tax, rounded up to the nearest .05
+// 3. the final price of the
+
 function addItem(name, price, type, imported, amount, cartArray){
     let item = {
         itemName: name,
@@ -36,27 +41,56 @@ function calculateSalesTax(cartObject) {
 }
 
 function calculateSalesTaxAmount(cartObject) {
-    console.log("Name: ", cartObject.itemName);
-    let taxRate = calculateSalesTax(cartObject);
-    console.log("Tax Rate: ", taxRate);
+    // console.log("Name: ", cartObject.itemName);
+    let taxRate = parseFloat(calculateSalesTax(cartObject));
+    // console.log("Tax Rate: ", taxRate);
     let subTotal = cartObject.itemPrice * cartObject.itemAmount;
-    console.log("Subtotal: ", subTotal);
+    // console.log("Subtotal: ", subTotal);
     // Need an if statement here to calculate sales tax if applicable
     if (taxRate > 0) {
-        let taxedTotal = subTotal + (subTotal * taxRate);
-        console.log("Taxed total: ", taxedTotal);
-        let roundedTotal = (Math.ceil(taxedTotal * 20) / 20)
-        return roundedTotal.toFixed(2)
+        let salesTax = (subTotal * taxRate);
+        let roundedTax = (Math.ceil(salesTax * 20) / 20)
+        // console.log("Taxed total: ", roundedTax.toFixed(2));
+        return [subTotal.toFixed(2), roundedTax.toFixed(2)]
     }
-    return subTotal.toFixed(2);
+    return [subTotal.toFixed(2), "0.00"];
 }
 
-let basket_1 = [];
+function calculateCartTotal(basket){
+    let finalSubtotal = 0;
+    let finalSalesTax = 0;
+    basket.forEach(function(cartObject){
+        var objectPrices = calculateSalesTaxAmount(cartObject);
+        console.log(cartObject.itemAmount + " " + cartObject.itemName + ": " + objectPrices[0]);
+        // console.log("Sales tax on " + cartObject.itemName + ": " + objectPrices[1]);
+        finalSubtotal += parseFloat(objectPrices[0]);
+        finalSalesTax += parseFloat(objectPrices[1]);
+    });
+    // console.log("Subtotal: " + finalSubtotal.toFixed(2));
+    console.log("Sales Taxes: " + finalSalesTax.toFixed(2));
+    console.log("Total: " + (finalSubtotal + finalSalesTax).toFixed(2));
+}
 
+
+
+let basket_1 = [];
 addItem("book", 12.49, "book", false, 1, basket_1);
 addItem("music CD", 14.99, "misc", false, 1, basket_1);
 addItem("chocolate bar", .85, "food", false, 1, basket_1);
-addItem("imported bottle of perfume", 47.50, "misc", true, 1, basket_1);
 
-console.log(basket_1);
+let basket_2 = [];
+addItem("imported box of chocolates", 10.00, "food", true, 1, basket_2);
+addItem("imported bottle of perfume", 47.50, "misc", true, 1, basket_2);
 
+let basket_3 = [];
+addItem("imported bottle of perfume", 27.99, "misc", true, 1, basket_3);
+addItem("bottle of perfume", 18.99, "misc", false, 1, basket_3);
+addItem("packet of headache pills", 9.75, "medical", false, 1, basket_3);
+addItem("box of imported chocolates", 11.25, "food", true, 1, basket_3);
+
+calculateCartTotal(basket_1);
+console.log("==================================================")
+calculateCartTotal(basket_2);
+console.log("==================================================")
+calculateCartTotal(basket_3);
+console.log("==================================================")
