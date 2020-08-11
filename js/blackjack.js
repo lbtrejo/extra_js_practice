c0883f34cc45f95422fe34b5ce9eaaa4369726ca
Levi Trejo <levi.b.trejo@gmail.com>
2020-08-10 22:07:36 -0500

    Progress on card logic

---
+++ js/blackjack.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// Need a deck of cards, an array seems like the right storage medium,
+// Cards will be A-K
+//
+
+let suit = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+let cardValues = {
+    "A": [1, 11],
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "J": 10,
+    "Q": 10,
+    "K": 10
+}
