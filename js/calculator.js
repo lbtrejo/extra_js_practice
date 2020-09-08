b395c382e63c9bc5ac5ad6a74d96ebc41179e2aa
Levi Trejo <levi.b.trejo@gmail.com>
2020-09-08 16:08:23 -0500

    feat: added inputBG function, activeField selector function, and operator function

--- js/calculator.js
+++ js/calculator.js
@@ -2,12 +2,53 @@
 
 $(document).ready(function(){
     // setup eventListeners for all of the buttons
+    let activeField = "";
 
-    $("#numberOne").click(function() {
-        // let currentValue = $("#numberInputOne").val();
-        // let buttonValue = $("#numberOne").val();
+    function calculateActiveField() {
+        let leftLength = $("#numberInputOne").val().length;
+        let midLength = $("#operatorInput").val().length;
 
-        // alert("Number 1 value is: " + buttonValue);
-        $("#numberInputOne").val($("#numberInputOne").val() + $("#numberOne").val());
+        if (leftLength > 0 && midLength > 0) {
+            activeField = $("#numberInputTwo");
+            return activeField;
+        } else {
+            activeField = $("#numberInputOne");
+            return activeField;
+        }
+    }
+
+    function inputBackground(active) {
+        $("input").removeClass("bg-white");
+        active.addClass("bg-white");
+    }
+
+    // function clickNumber(){
+    //     calculateActiveField();
+    //     inputBackground(activeField);
+    //     return activeField.val(activeField.val() + $(this).val());
+    // }
+
+    // function clickOperator(){
+    //     activeField = $("#operatorInput");
+    //     inputBackground(activeField);
+    //     activeField.val($(this).val())
+    // }
+
+    $("#numberOne").click(function(){
+        calculateActiveField();
+        inputBackground(activeField);
+        activeField.val(activeField.val() + $(this).val());
+    })
+
+    $("#numberTwo").click(function() {
+        calculateActiveField();
+        inputBackground(activeField);
+        activeField.val(activeField.val() + $(this).val());
+    })
+
+    $("#operatorPlus").click(function(){
+        activeField = $("#operatorInput");
+        inputBackground(activeField);
+        $("#operatorInput").val($(this).val())
     })
 });
