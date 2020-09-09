6e256acd4fc3f6c514c8ba2c0bc21c235791ef83
Levi Trejo <levi.b.trejo@gmail.com>
2020-09-09 10:23:56 -0500

    feat: number and operator buttions fully functional

--- js/calculator.js
+++ js/calculator.js
@@ -1,18 +1,23 @@
 "use strict";
 
 $(document).ready(function(){
-    // setup eventListeners for all of the buttons
+
+    const inputOne = $("#numberInputOne");
+    const inputTwo = $("#numberInputTwo");
+    const inputOperator = $("#operatorInput");
     let activeField = "";
 
     function calculateActiveField() {
-        let leftLength = $("#numberInputOne").val().length;
-        let midLength = $("#operatorInput").val().length;
-
+        let leftLength = inputOne.val().length;
+        let midLength = inputOperator.val().length;
+        // let rightLength = inputTwo.val().length;
+        console.log("leftLength:", leftLength);
+        console.log("midLength:", midLength);
         if (leftLength > 0 && midLength > 0) {
-            activeField = $("#numberInputTwo");
+            activeField = inputTwo;
             return activeField;
         } else {
-            activeField = $("#numberInputOne");
+            activeField = inputOne;
             return activeField;
         }
     }
@@ -22,33 +27,33 @@ $(document).ready(function(){
         active.addClass("bg-white");
     }
 
-    // function clickNumber(){
-    //     calculateActiveField();
-    //     inputBackground(activeField);
-    //     return activeField.val(activeField.val() + $(this).val());
-    // }
-
-    // function clickOperator(){
-    //     activeField = $("#operatorInput");
-    //     inputBackground(activeField);
-    //     activeField.val($(this).val())
-    // }
-
-    $("#numberOne").click(function(){
+    function clickNumber(){
         calculateActiveField();
         inputBackground(activeField);
         activeField.val(activeField.val() + $(this).val());
-    })
+        console.log($(this));
+    }
 
-    $("#numberTwo").click(function() {
-        calculateActiveField();
-        inputBackground(activeField);
-        activeField.val(activeField.val() + $(this).val());
-    })
-
-    $("#operatorPlus").click(function(){
+    function clickOperator(){
         activeField = $("#operatorInput");
         inputBackground(activeField);
-        $("#operatorInput").val($(this).val())
-    })
+        activeField.val($(this).val());
+    }
+
+    $("#numberOne").click(clickNumber);
+    $("#numberTwo").click(clickNumber);
+    $("#numberThree").click(clickNumber);
+    $("#numberFour").click(clickNumber);
+    $("#numberFive").click(clickNumber);
+    $("#numberSix").click(clickNumber);
+    $("#numberSeven").click(clickNumber);
+    $("#numberEight").click(clickNumber);
+    $("#numberNine").click(clickNumber);
+    $("#numberZero").click(clickNumber);
+
+    $("#operatorPlus").click(clickOperator)
+    $("#operatorMinus").click(clickOperator);
+    $("#operatorMultiply").click(clickOperator);
+    $("#operatorDivide").click(clickOperator);
+
 });
